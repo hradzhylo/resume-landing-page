fe4a7d8539528c7122b8489a4896f6ed773597d2
volodymyr <hradzhylo.volodymyr@gmail.com>
2018-11-02 16:33:14 +0200

    Initial commit.

---
+++ web/themes/custom/pentsaktheme/gulpfile.js
@@ -0,0 +1,112 @@
+'use strict';
+
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    prefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rigger = require('gulp-rigger'),
+    cssmin = require('gulp-minify-css'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant');
+
+var path = {
+    build: {
+        //html: 'build/',
+        js: './js/pentsak/',
+        //img: 'build/img/',
+        //fonts: 'build/fonts/',
+        css: './css/'
+    },
+    src: {
+        //html: 'src/*.html',
+        js: './js/resume.js',
+        //img: 'src/img/**/*.*',
+        //fonts: 'src/fonts/**/*.*',
+        style: './scss/style.scss'
+    },
+    watch: {
+        //html: '*.html',
+        js: './js/**/*.js',
+        //img: 'src/img/**/*.*',
+        //fonts: 'src/fonts/**/*.*',
+        style: './scss/**/*.scss'
+    },
+    clean: './build'
+};
+
+
+// gulp.task('html:build', function () {
+//     gulp.src(path.src.html)
+//         .pipe(rigger())
+//         .pipe(gulp.dest(path.build.html));
+// });
+
+gulp.task('js:build', function () {
+    gulp.src(path.src.js)
+        .pipe(rigger())
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.js));
+});
+
+// gulp.task('image:build', function () {
+//     gulp.src(path.src.img)
+//         .pipe(imagemin({
+//             progressive: true,
+//             svgoPlugins: [{removeViewBox: false}],
+//             use: [pngquant()],
+//             interlaced: true
+//         }))
+//         .pipe(gulp.dest(path.build.img));
+// });
+
+// gulp.task('fonts:build', function() {
+//     gulp.src(path.src.fonts)
+//         .pipe(gulp.dest(path.build.fonts))
+// });
+
+gulp.task('style:build', function () {
+    gulp.src(path.src.style)
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            sourceMap: true,
+            errLogToConsole: true
+        }))
+        .pipe(prefixer())
+        .pipe(cssmin())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.css));
+});
+
+gulp.task('build', [
+    // 'html:build',
+    'js:build',
+    // 'fonts:build',
+    // 'image:build',
+    'style:build'
+]);
+
+
+gulp.task('watch', function(){
+    // watch([path.watch.html], function(event, cb) {
+    //     gulp.start('html:build');
+    // });
+    watch([path.watch.js], function(event, cb) {
+        gulp.start('js:build');
+    });
+    // watch([path.watch.img], function(event, cb) {
+    //     gulp.start('image:build');
+    // });
+    // watch([path.watch.fonts], function(event, cb) {
+    //     gulp.start('fonts:build');
+    // });
+    watch([path.watch.style], function(event, cb) {
+        gulp.start('style:build');
+    });
+});
+
+
+gulp.task('default', ['build', 'watch']);
---
+++ web/themes/custom/pentsaktheme/js/resume.js
@@ -0,0 +1,41 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    // Select all links with hashes
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, "easeInOutExpo");
+                }
+            }
+        });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('a[href*="#"]').click(function() {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#navbar'
+    });
+
+})(jQuery); // End of use strict
